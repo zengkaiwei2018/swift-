f2367f83de0ba78732d1b6f07ffada7230b987d6
zengkaiwei2018 <1912207369@qq,com>
2019-09-18 11:47:40 +0800

    输出字符串数组

--- swift练习/main.swift
+++ swift练习/main.swift
@@ -1,7 +1,4 @@
- var array = [Int]()
- array.append(10)
- array.append(20)
- array += [30]
- for item in array{
+var strs = ["3","a","8","haha","100","hello"]
+for item in strs{
     print(item)
- }
+}
