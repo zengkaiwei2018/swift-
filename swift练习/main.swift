0a23267ceca6c04d9f7b11335e630c096182f7f8
zengkaiwei2018 <1912207369@qq,com>
2019-09-20 09:21:09 +0800

    将数组向左移动一步

--- swift练习/main.swift
+++ swift练习/main.swift
@@ -1,4 +1,14 @@
-var strs = ["3","a","8","haha","100","hello"]
+var strs:[String] = ["3","a","8","haha","100","hello"]
+for(index,item) in strs.enumerated(){
+    if  index < strs.count / 2
+    {
+        var temp = ""
+        temp = strs[index]
+        strs[index] = strs[strs.count - index - 1]
+        strs[strs.count - index - 1]
+    }
+}
+
 for item in strs{
-    print(item)
+    
 }
